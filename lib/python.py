fe21b87a3b18ab9e8d2c5cd92ec6542ff407408b
apronoy <apronoy17@gmail.com>
2025-11-19 20:33:46 +0600

    prectice python

---
+++ lib/python.py
@@ -0,0 +1,14 @@
+# print('Hi Pronoy !!')
+# if 1>2 :
+#  print('1 is the big number ')
+# else:
+#  print('1 is not big then 2')
+
+
+a = int(input('Enter your age : '))
+# if(a>17):
+#  print('adult ! welcome to the bar !!')
+# else:
+#  print('Wow, try after getting adult baby !!')
+
+print(type)
